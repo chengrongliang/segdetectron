321c83a2a2431458888b0ba0b848067385b694fb
master <chengrongliang@hotmail.com>
2018-01-25 11:59:03 +0800

    add loss

---
+++ demo/00_run_demo.sh
@@ -0,0 +1,5 @@
+python2 ../tools/infer_simple.py    \
+         --cfg ../configs/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml \
+         --output-dir results     --image-ext jpg \
+         --wts /home/lesee/work/detectron/demo/model/model_final.pkl     \
+         test_image
